c486d39e6dda889bdc03722bcc41ba5a4a399b91
eaturspinach <markmuskardin@gmail.com>
2011-10-05 16:24:47 -0700

    added new look and feel - some fixes for interactive show input

--- public/javascripts/app/views/neuron_form_view.js
+++ public/javascripts/app/views/neuron_form_view.js
@@ -1,28 +1,27 @@
 (function($){
   
   window.NeuronFormView = Backbone.View.extend({
-    tagName: 'textarea',
-    id: 'neuron_text_area',
+    tagName: 'input',
+    id: 'neuron_input',
     hook: '#neuron_form',
     neurons_list: $('#neurons'),
-    neuron_text_area: $('#neuron_text_area'),
     template: _.template($('#neuron_form_template').html()),
     events: {
-      'input':'showInput'
+      'input':'showInteractiveInput'
     },
     initialize: function(){
       _.bindAll(this,'render');
     },
     render: function(){
       var renderedContent = $(this.el).html(this.template);
-      $(this.hook).html(renderedContent);
+      $(this.hook).append(renderedContent);
       return this;
     },
-    showInput: function(){
-      if($(this.neurons_list).children(':first').attr('class')){
-        $(this.neurons_list).prepend('<li id="temp-input">' + $('#neuron_text_area').val() + '</li>');
+    showInteractiveInput: function(){
+      if($(this.neurons_list).children(':first').attr('class') == 'neuron'){
+        $(this.neurons_list).prepend('<li id="temp-input">' + $('#neuron_input').val() + '</li>');
       } else {
-        $(this.neurons_list).children('#temp-input').html($('#neuron_text_area').val());
+        $(this.neurons_list).children('#temp-input').html($('#neuron_input').val());
       }
     }
   })
